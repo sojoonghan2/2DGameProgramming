1c9849e08946c44dd36542f4867b1e32c5944b1c
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-14 19:19:57 +0900

    Drill 1 제출 (2020180016 민경원)

---
+++ Drill01/Turtle.py
@@ -0,0 +1,29 @@
+import turtle
+
+def w_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def s_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+def a_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def d_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+turtle.stamp()
+turtle.onkey(w_move, 'w')
+turtle.onkey(s_move, 's')
+turtle.onkey(a_move, 'a')
+turtle.onkey(d_move, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
