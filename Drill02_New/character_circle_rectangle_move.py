9af8dee0d4a204de4583f79865c19257d68126fa
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-21 10:35:44 +0900

    함수화 성공

--- Drill02_New/character_circle_rectangle_move.py
+++ Drill02_New/character_circle_rectangle_move.py
@@ -6,26 +6,32 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x, y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.01)
+
 def run_circle():
     cx, cy, r = 400, 300, 200
-    for deg in range(0, 360, 5):
+    for deg in range(-90, 270, 5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_frame(x, y)
         
 def run_rectangle():
+    # bottom
     for x in range(50, 750 + 1, 10):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        delay(0.01)
-    pass
+        render_frame(x, 90)
 
+        
+    #top
+    for x in range(750, 50, -10):
+        render_frame(x, 550)
+
+    
 while True:
-    # run_circle()
     run_rectangle()
+    run_circle()
 
 close_canvas()
