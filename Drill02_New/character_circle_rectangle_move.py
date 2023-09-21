17c7a13030205820bd9bed507d22fefb5600e835
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-21 09:58:14 +0900

    원, 사각형 무한루프 뼈대 생성

--- Drill02_New/character_circle_rectangle_move.py
+++ Drill02_New/character_circle_rectangle_move.py
@@ -5,4 +5,15 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    print('Circle')
+    pass
+def run_rectangle():
+    print('Rectangle')
+    pass
+
+while(True):
+    run_circle()
+    run_rectangle()
+
 close_canvas()
