1c92d8a4a138ede568d2e700232038c2befc62b8
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-23 16:10:05 +0900

    Drill 3 예습

--- Drill03/character_runs.py
+++ Drill03/character_runs.py
@@ -1,16 +1,26 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
-x = 0
+character = load_image('sp_image.png')
+
+x = 800
 frame = 0
-while (x < 800):
+switch = 0
+while (x > 0):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    if (switch < 6):
+        character.clip_draw(frame * 90, 310, 90, 70, x, 70)
+        switch += 1
+    if (switch < 12 and switch >= 6):
+        character.clip_draw(frame * 90, 180, 90, 70, x, 70)
+        switch += 1
+    if (switch >= 12):
+        switch = 0
     update_canvas()
-    frame = (frame + 1) % 8
-    x += 5
+    frame = (frame + 1) % 6
+    x -= 5
     delay(0.05)
     get_events()
+
 close_canvas()
