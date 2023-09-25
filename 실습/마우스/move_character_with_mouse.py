d89ca93eed0086f1c6350a246864980ecc0c5835
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-25 15:08:52 +0900

    실습 추가

---
+++ 실습/마우스/move_character_with_mouse.py
@@ -0,0 +1,45 @@
+from pico2d import *
+
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+
+character = load_image('animation_sheet.png')
+
+
+def handle_events():
+    global running, x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+running = True
+frame = 0
+
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+hide_cursor()
+
+
+
+while running:
+    clear_canvas()
+
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+close_canvas()
+
+
+
+
--- 실습/키보드/character_moves.py
+++ 실습/키보드/character_moves.py
@@ -2,13 +2,12 @@ from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
-x = 0
-while (x < 800):
+
+for x in range(0, 800, 5):
     clear_canvas()
     grass.draw(400, 30)
     character.draw(x, 90)
-    x = x + 2
     update_canvas()
     delay(0.01)
-    get_events()
+
 close_canvas()
