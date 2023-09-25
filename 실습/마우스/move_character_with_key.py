5cbeb85646b3ca052bd7928a7ae9d5cd1cde5f81
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-09-25 19:39:28 +0900

    실습 추가

--- 실습/마우스/move_character_with_key.py
+++ 실습/마우스/move_character_with_key.py
@@ -7,7 +7,7 @@ character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running
+    global running, dir
 
     # fill here
 
@@ -15,14 +15,35 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
-# fill here
+while running:
+clear_canvas()
+grass.draw(400, 30)
+character.clip_draw(frame*100,100,100,100,x,90)
+update_canvas()
+handle_events()
+frame = (frame + 1) % 8
+x += dir * 5
+delay(0.05)
+
 
 
 close_canvas()
