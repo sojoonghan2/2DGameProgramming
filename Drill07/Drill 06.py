bee813f8fee6291e10f4e90c5e0666e60130ba9c
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-10-02 15:59:58 +0900

    마우스 클릭에 따라 손 위치를 추가.

--- Drill07/Drill 06.py
+++ Drill07/Drill 06.py
@@ -21,6 +21,9 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
+        # 클릭된 위치를 새로운 점으로 추가
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            points.append((mx, my))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -38,7 +41,7 @@ def reset_World():
     frame = 0
     action = 3
 
-    points = [ (100, 900), (1200, 800), (500, 100) ]
+    points = [  ]
     set_new_target_arrow()
 
 
