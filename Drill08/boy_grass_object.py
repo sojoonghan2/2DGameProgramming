7308080bc1e48553877cd3ad114bfef281892f34
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-10-05 10:41:15 +0900

    소년 클래스 생성

--- Drill08/boy_grass_object.py
+++ Drill08/boy_grass_object.py
@@ -7,10 +7,24 @@ class Grass:
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image.draw(400, 300)
+        self.image.draw(400, 30)
 
     def update(self):
         pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
 
 
 def handle_events():
