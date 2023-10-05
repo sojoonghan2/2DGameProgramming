16872712180080e8339fbccf4aec54bd6c7e8ecc
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-10-05 11:19:04 +0900

    Drill 8 제출 (2020180016 민경원)

--- Drill08/boy_grass_object.py
+++ Drill08/boy_grass_object.py
@@ -28,6 +28,34 @@ class Boy:
         self.x += 5
 
 
+class SmallBall:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.frame = random.randint(0, 7)
+        self.image = load_image('ball21x21.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        if self.y > 50:
+            self.y -= random.randint(5, 10)
+
+
+class BigBall:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.frame = random.randint(0, 7)
+        self.image = load_image('ball41x41.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        if self.y > 60:
+            self.y -= random.randint(5, 10)
+
+
 def handle_events():
     global running
     events = get_events()
@@ -42,6 +70,8 @@ def reset_world():
     global running
     global grass
     global team
+    global smallball
+    global bigball
     global world
 
     running = True
@@ -52,6 +82,15 @@ def reset_world():
 
     team = [Boy() for i in range(10)]
     world += team
+
+    smallball_num = random.randint(0, 20)
+    bigball_num = 20 - smallball_num
+
+    smallball = [SmallBall() for i in range(smallball_num)]
+    world += smallball
+
+    bigball = [BigBall() for i in range(bigball_num)]
+    world += bigball
 
 
 def update_world():
