dc1d73a39206cef36592ea3e28c21e9cd4204290
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-11-02 11:24:06 +0900

    Drill 11 제출 (2020180016 민경원)

---
+++ Drill11/bird.py
@@ -0,0 +1,137 @@
+# 이것은 각 상태들을 객체로 구현한 것임.
+
+from pico2d import get_time, load_image, load_font, clamp,  SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
+from ball import Ball, BigBall
+import game_world
+import game_framework
+import random
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+# state event check
+# ( state event type, event value )
+
+# bird Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# bird Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+class Idle:
+
+    @staticmethod
+    def enter(bird, e):
+        if bird.face_dir == -1:
+            bird.action = 2
+        elif bird.face_dir == 1:
+            bird.action = 3
+        bird.dir = 0
+        bird.frame = 0
+        bird.wait_time = get_time() # pico2d import 필요
+        pass
+
+    @staticmethod
+    def exit(bird, e):
+        pass
+
+    @staticmethod
+    def do(bird):
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        bird.x += bird.dir * RUN_SPEED_PPS * game_framework.frame_time
+
+
+    @staticmethod
+    def draw(bird):
+        bird.image.clip_draw(int(bird.frame) * 181, bird.action, 180, 150, bird.x, bird.y)
+
+class Run:
+
+    @staticmethod
+    def enter(bird, e):
+        if right_down(e) or left_up(e): # 오른쪽으로 RUN
+            bird.dir, bird.action, bird.face_dir = 1, 1, 1
+        elif left_down(e) or right_up(e): # 왼쪽으로 RUN
+            bird.dir, bird.action, bird.face_dir = -1, 0, -1
+
+    @staticmethod
+    def exit(boy, e):
+        if space_down(e):
+            boy.fire_ball()
+
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        boy.x += boy.dir * RUN_SPEED_PPS * game_framework.frame_time
+
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(int(boy.frame) * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+class StateMachine:
+    def __init__(self, bird):
+        self.bird = bird
+        self.cur_state = Idle
+        self.transitions = {
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run}
+        }
+
+    def start(self):
+        self.cur_state.enter(self.bird, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.bird)
+
+    def handle_event(self, e):
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.bird)
+
+class Bird:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 1500), 450
+        self.frame = 0
+        self.action = 4
+        self.face_dir = 1
+        self.dir = 0
+        self.image = load_image('bird_animation.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
+
+    def draw(self):
+        self.state_machine.draw()
