dc0a55deaaf0bb0ad15c5f54ae275d137379aec2
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-11-03 09:35:40 +0900

    게임 월드 수정

--- Drill11/play_mode.py
+++ Drill11/play_mode.py
@@ -4,7 +4,6 @@ import game_framework
 import game_world
 from grass import Grass
 from boy import Boy
-from bird import Bird
 
 # boy = None
 
@@ -21,7 +20,6 @@ def handle_events():
 def init():
     global grass
     global boy
-    global bird
 
     running = True
 
@@ -30,27 +28,6 @@ def init():
 
     boy = Boy()
     game_world.add_object(boy, 1)
-
-    bird1 = Bird()
-    game_world.add_object(bird1, 1)
-    bird2 = Bird()
-    game_world.add_object(bird2, 1)
-    bird3 = Bird()
-    game_world.add_object(bird3, 1)
-    bird4 = Bird()
-    game_world.add_object(bird4, 1)
-    bird5 = Bird()
-    game_world.add_object(bird5, 1)
-    bird6 = Bird()
-    game_world.add_object(bird6, 1)
-    bird7 = Bird()
-    game_world.add_object(bird7, 1)
-    bird8 = Bird()
-    game_world.add_object(bird8, 1)
-    bird9 = Bird()
-    game_world.add_object(bird9, 1)
-    bird10 = Bird()
-    game_world.add_object(bird10, 1)
 
 
 def finish():
--- Potato Shoot/play_mode.py
+++ Potato Shoot/play_mode.py
@@ -1,5 +1,6 @@
 from pico2d import *
-
+import game_framework
+import game_world
 from ground import Ground
 from potato import Potato
 from bottle import Bottle
@@ -11,14 +12,13 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            game_framework.quit()
         else:
             potato.handle_event(event)
 
-
-def reset_world():
+def init():
     global running
     global ground
     global team
@@ -32,66 +32,63 @@ def reset_world():
 
     # 땅
     ground = Ground()
-    world.append(ground)
+    game_world.add_object(ground, 0)
 
     # 물
     water1 = Water(-230, -120)
-    world.append(water1)
+    game_world.add_object(water1, 1)
     water2 = Water(230, -120)
-    world.append(water2)
+    game_world.add_object(water2, 1)
 
     # 병
     # 4열 4개
     bottle1 = Bottle(80, 800)
-    world.append(bottle1)
+    game_world.add_object(bottle1, 2)
     bottle2 = Bottle(160, 800)
-    world.append(bottle2)
+    game_world.add_object(bottle2, 2)
     bottle3 = Bottle(240, 800)
-    world.append(bottle3)
+    game_world.add_object(bottle3, 2)
     bottle4 = Bottle(320, 800)
-    world.append(bottle4)
+    game_world.add_object(bottle4, 2)
     # 3열 3개
     bottle5 = Bottle(120, 770)
-    world.append(bottle5)
+    game_world.add_object(bottle5, 2)
     bottle6 = Bottle(200, 770)
-    world.append(bottle6)
-    bottle4 = Bottle(280, 770)
-    world.append(bottle4)
+    game_world.add_object(bottle6, 2)
+    bottle7 = Bottle(280, 770)
+    game_world.add_object(bottle7, 2)
     # 2열 2개
-    bottle5 = Bottle(160, 740)
-    world.append(bottle5)
-    bottle6 = Bottle(240, 740)
-    world.append(bottle6)
+    bottle8 = Bottle(160, 740)
+    game_world.add_object(bottle8, 2)
+    bottle9 = Bottle(240, 740)
+    game_world.add_object(bottle9, 2)
     # 1열 1개
-    bottle7 = Bottle(200, 710)
-    world.append(bottle7)
+    bottle10 = Bottle(200, 710)
+    game_world.add_object(bottle10, 2)
 
     # 감자
     potato = Potato(250, 100)
-    world.append(potato)
+    game_world.add_object(potato, 2)
 
 
-
-def update_world():
-    for o in world:
-        o.update()
+def finish():
+    game_world.clear()
     pass
 
 
-def render_world():
+def update():
+    game_world.update()
+    # delay(0.1)
+
+
+def draw():
     clear_canvas()
-    for o in world:
-        o.draw()
+    game_world.render()
     update_canvas()
 
+def pause():
+    pass
 
-open_canvas(540, 960)
-reset_world()
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.01)
-# finalization code
-close_canvas()
+def resume():
+    pass
+
