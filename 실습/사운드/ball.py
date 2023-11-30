6cbab8fe0c70e0d3fce9a34cd821fff6445bd7f8
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-11-30 10:00:55 +0900

    사운드 실습 완료

--- 실습/사운드/ball.py
+++ 실습/사운드/ball.py
@@ -5,18 +5,23 @@ import game_world
 
 import server
 
+
 class Ball:
     image = None
     zombie_eat_sound = None
     boy_eat_sound = None
 
-    def __init__(self, x = None, y = None):
+    def __init__(self, x=None, y=None):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
         self.x = x if x else random.randint(100, server.background.w - 100)
         self.y = y if y else random.randint(100, server.background.h - 100)
 
-        # fill here
+        if not Ball.zombie_eat_sound:
+            Ball.zombie_eat_sound = load_wav('zombie_pickup.wav')
+            Ball.boy_eat_sound = load_wav('pickup.wav')
+            Ball.zombie_eat_sound.set_volume(32)
+            Ball.boy_eat_sound.set_volume(32)
 
     def draw(self):
         self.image.draw(self.x - server.background.window_left, self.y - server.background.window_bottom)
@@ -30,5 +35,8 @@ class Ball:
     def handle_collision(self, group, other):
         match group:
             case 'boy:ball':
-                # fill here
+                Ball.boy_eat_sound.play()
+                game_world.remove_object(self)
+            case 'zombie:ball':
+                Ball.zombie_eat_sound.play()
                 game_world.remove_object(self)
